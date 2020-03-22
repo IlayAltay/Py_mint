933df289c4a4c7491c3c06ae9fcbb52238589d24
IlayAltay <petrogonsales@mail.ru>
2020-03-22 11:08:31 +0300

    удаление не рабочих папок

--- TestBotV1/testbotV1.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot('1023899509:AAEX4KXDYer8DW8ZGxOxFQ4fStWwgTJrAeA')
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'dfdfgdfgfgdgfdgf /start')
-
-bot.polling()
